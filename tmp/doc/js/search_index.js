58fc60766c770a54a51f84b5e0ae576a1b23be52
Jan Jezek <mail@mediatainment-productions.com>
2013-11-04 11:45:28 +0100

    documentation

---
+++ tmp/doc/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["readme"],"longSearchIndex":[""],"info":[["README","","README_rdoc.html","","<p>code_highlight\n<p>This is an small extension for redmine, which adds a code highlight button\nto the default ...\n"]]}}
